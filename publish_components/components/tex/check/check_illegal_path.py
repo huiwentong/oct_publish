795bd128f8f6df41c22f9b92c5bca8e2594dbcce
gongshuai <gongshuai@163.com>
2026-08-27 15:06:19 +0800

    add check dir path  for check_illegal_path

--- publish_components/components/tex/check/check_illegal_path.py
+++ publish_components/components/tex/check/check_illegal_path.py
@@ -5,19 +5,22 @@ import pymel.core as pm
 
 def main(submit_data:dict, process_data:dict, parent_widget=None, logger=None):
     """
-    检查 贴图文件名
+    检查 贴图文件路径 不应该有中文，空格，特殊字符
     """
     try:
-        p = re.compile('.*[^\w\.]')
+        name_pattern = re.compile(r'^[A-Za-z0-9_.-]+$')
+        folder_pattern = re.compile(r'^[A-Za-z0-9_.:\\/-]+$')
         errs = ''
         for n_f in pm.ls(type=['file', 'aiImage']):
             try:
                 tex_path = n_f.fileTextureName.get()
             except:
                 tex_path = n_f.filename.get()
-            f_name = os.path.basename(tex_path)
-            if p.match(f_name.upper().replace('.<UDIM>', '').replace('_<UDIM>', '')):
-                errs += '{}: {}\n'.format(n_f.name(), f_name)
+            tex_name = os.path.basename(tex_path)
+            check_name = tex_name.upper().replace('.<UDIM>', '').replace('_<UDIM>', '')
+            tex_dir = os.path.dirname(tex_path)
+            if not bool(name_pattern.fullmatch(check_name)) or not bool(folder_pattern.fullmatch(tex_dir)):
+                errs += '{}: {}\n'.format(n_f.name(), tex_path)
         if errs:
             return u"贴图文件名应该由数字字母下划线组成，不应该有中文，空格，特殊字符：\n" + errs
     except:
